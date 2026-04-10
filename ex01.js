364caa12bba4b2263477b80b7a9335937fbb9211
joao <agredajoaofelipe@gmail.com>
2026-04-10 17:23:18 -0300

    Add files via upload

---
+++ ex01.js
@@ -0,0 +1,48 @@
+//criar uma variável chamada 'nome' e mostrar:
+//"Meu nome é ______"
+
+let nome = 'Joao';
+//console.log('Meu nome é ' + nome);
+console.log(`Meu nome é ${nome}`);
+
+//criar uma variavel idade e msotrar:
+//"Eu tenho _____ anos de idade"
+let idade = 18;
+console.log(`Eu tenho ${idade} anos de idade`)
+//console.log(' Eu tenho ' + idade + 'anod de idade');
+
+//criar duas variaveis 'cidade' e 'estado' e exiba:
+//Eu moro em _____ - ______
+
+let cidade = 'maringa'
+let estado = 'parana'
+console.log(`Eu moro em ${cidade} - ${estado}`);
+
+//Criar duas variaveis e mostre:
+//a soma, a subtraçao, a multiplicaçaõ, a divisão, o dobro, o triplo,a metade e  o quadrado
+let N1 = 5
+let N2 = 5 
+let soma = N1 + N2;
+console.log(`a soma de ${N1} e ${N2} e ${soma}`)
+let mult = N1 * N2;
+console.log(`a subtração de ${N1} e ${N2} e ${mult}`)
+let divisao = N1 / N2;
+console.log(`a divisão de ${N1} e ${N2} e ${divisao}`)
+let sub = N1 - N2;
+console.log(`a subtração de ${N1} e ${N2} e ${sub}`)
+let dobro = N1 + N1;
+console.log(`o dobro DE ${N1} e ${N2} e ${dobro}`)
+let triplo = N1*3;
+console.log(`o triplo de ${N1} e ${N2} e  ${triplo}`)
+let met = N1 / 2;
+console.log(`a metade de ${N1} e ${N2}  e ${met}`)
+let quadra = N1** 2;
+console.log(`o quadrado de ${N1} e ${N2} e ${quadra}`)
+
+
+
+
+
+
+
+
---
+++ ex02.js
@@ -0,0 +1,41 @@
+//verificar se um numero e maior que 10
+let num = 15;
+if (num > 10) {
+    console.log(`O numero ${num} é maior que 10`);
+} else {
+console.log('menor')
+}
+
+//verifique se um numero e par ou impar 
+let num2 = 1
+if (num2 % 2 === 0) {
+    console.log("par")
+} else {
+console.log("impar")
+}
+
+//verificar se um numero e positivo
+let num3 = 5;
+if(num3 >=0) {
+    console.log('positivo')
+}else {
+console.log('negativo')
+}
+
+let temp = 15;
+if (temp <=15) {
+console.log('frio')
+}else if (temp >=16 && temp <=25) {
+console.log('agradavel')
+}else {
+    console.log('quente')
+}
+
+let idade = 12;
+if (idade <12) {
+    console.log('criança')
+} else if (idade <=12 && idade <=17) {
+    console.log('adolescente')
+}else {
+    console.log( 'adulto')
+}
